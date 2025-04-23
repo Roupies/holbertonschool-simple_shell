8dfe4543ce0366f7a4dc1519558892e786a07d8a
‘Roupies’ <maxime.naguet@gmail.com>
2025-04-23 13:56:13 +0200

    aaa

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-int last_exit_status;
+int last_exit_status;  /* Variable globale pour stocker le statut de sortie */
 
 /**
  * display_prompt - Print shell prompt
@@ -21,18 +21,22 @@ int handle_builtins(char **args)
 {
 	int i;
 
+	/* Si la commande est 'exit' */
 	if (_strcmp(args[0], "exit") == 0)
 	{
-		exit(0);
+		/* Retourner -1 pour signaler que 'exit' a été appelé */
+		return (-1);
 	}
 
+	/* Si la commande est 'env', on l'affiche */
 	if (_strcmp(args[0], "env") == 0)
 	{
 		for (i = 0; environ[i] != NULL; i++)
 			printf("%s\n", environ[i]);
 		return (1);
 	}
-	return (0);
+
+	return (0);  /* Pas un builtin reconnu */
 }
 
 /**
@@ -55,6 +59,7 @@ int handle_input(char *line, char **av)
 		return (0);
 	}
 
+	/* Si l'utilisateur demande le code de sortie $? */
 	if (strcmp(args[0], "$?") == 0)
 	{
 		printf("%d\n", last_exit_status);
@@ -62,18 +67,20 @@ int handle_input(char *line, char **av)
 		return (0);
 	}
 
+	/* Gérer les builtins */
 	builtin_result = handle_builtins(args);
 	if (builtin_result == -1)
 	{
 		free_args(args);
-		return (-1);
+		return (-1);  /* Sortir si exit est appelé */
 	}
 	else if (builtin_result == 1)
 	{
 		free_args(args);
-		return (0); /* Builtin executed, skip command execution */
+		return (0);  /* Si un autre builtin est exécuté, on continue */
 	}
 
+	/* Exécuter la commande normale et mettre à jour last_exit_status */
 	last_exit_status = execute_command(args, av[0]);
 	free_args(args);
 	return (0);
@@ -104,6 +111,10 @@ int main(int ac, char **av)
 		if (handle_input(line, av) == -1)
 			break;
 	}
+
 	free(line);
-	return (0);
+
+	/* Retourner le statut de sortie de la dernière commande avant exit */
+	return (last_exit_status);
 }
+
