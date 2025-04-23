08b7c96c3a50bcf458190549023922f030268be1
‘Roupies’ <maxime.naguet@gmail.com>
2025-04-23 11:31:34 +0200

    aaa

--- executor.c
+++ executor.c
@@ -15,16 +15,22 @@ int execute_command(char **args, char *prog_name)
 	int exit_status;
 	int signal_num;
 
-	if (args == NULL || args[0] == NULL)
+	/* Check for null args */
+	if (!args || !args[0])
+	{
+		fprintf(stderr, "%s: command not found\n", prog_name);
 		return (127);
+	}
 
+	/* Get the full path of the command */
 	cmd_path = find_in_path(args[0]);
-	if (cmd_path == NULL)
+	if (!cmd_path)
 	{
 		fprintf(stderr, "%s: %s: not found\n", prog_name, args[0]);
 		return (127);
 	}
 
+	/* Fork the process */
 	pid = fork();
 	if (pid == -1)
 	{
@@ -33,9 +39,9 @@ int execute_command(char **args, char *prog_name)
 		return (127);
 	}
 
+	/* Child process */
 	if (pid == 0)
 	{
-		/* Child process executes the command */
 		if (execve(cmd_path, args, environ) == -1)
 		{
 			perror("execve");
@@ -45,7 +51,6 @@ int execute_command(char **args, char *prog_name)
 	}
 	else
 	{
-		/* Parent process waits for child */
 		waitpid(pid, &status, 0);
 		free(cmd_path);
 
